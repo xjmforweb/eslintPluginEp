edce44f1f416f8f471ea294521ee97b4b28698df
xjm <zkf15138@163.com>
2023-08-28 13:43:58 +0800

    1

---
+++ tests/lib/rules/getChinese.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview lang
+ * @author lang
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/getChinese"),
+    RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+ruleTester.run("lang", rule, {
+    valid: [
+        // give me some code that won't trigger a warning
+    ],
+
+    invalid: [
+        {
+            code: "",
+            errors: [{ message: "Fill me in.", type: "Me too" }],
+        },
+    ],
+});
