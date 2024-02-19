cfd44a354fbb9843c170cd2f063babbf049a396b
xjm <zkf15138@163.com>
2024-02-19 13:16:34 +0800

    react test

--- tests/lib/rules/lang.js
+++ tests/lib/rules/lang.js
@@ -8,24 +8,35 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-const rule = require("../../../lib/rules/lang"),
-  RuleTester = require("eslint").RuleTester;
-
+const rule = require("../../../lib/rules/setReactLang")
+const RuleTester = require('eslint').RuleTester
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  }
+  // parser: require('@babel/eslint-parser')
+})
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
 ruleTester.run("lang", rule, {
   valid: [
     // give me some code that won't trigger a warning
+    `
+        <Table.Column title={<Cell label='医嘱类型' code='hospitalizationInformation:physicianOrderSheet:orderType' disabled={tableData.every(v => !v.repeatIndicator)} />} dataIndex="repeatIndicator" render={_ => ( <Valuex code='hospitalizationInformation:physicianOrderSheet:orderType' d={_} />)} />
+    `
   ],
 
   invalid: [
-    {
-      code: "",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
-    },
+    // {
+      // code: ``,
+      // errors: [{ message: "Fill me in.", type: "Me too" }],
+    // },
   ],
 });
